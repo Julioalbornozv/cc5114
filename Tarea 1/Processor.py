0427558648ef796a3208f08a08a7d2f75a646b72
Julioalbornozv <julio.albornoz@ug.uchile.cl>
2019-08-31 17:02:39 -0400

    Added Requested Plots

--- Tarea 1/Processor.py
+++ Tarea 1/Processor.py
@@ -1,7 +1,8 @@
 import Network as ntk
 import Perceptron as pc
-import numpy as np
 import Functions as fc
+import numpy as np
+import matplotlib.pyplot as plt
 
 """
 TODO: (Post-release)
@@ -27,13 +28,17 @@ for i in range(len(dataset[0])-1):
 	
 ### One-hot encoding
 expected = []
+LA = np.array([1,0,0])
+LB = np.array([0,1,0])
+LC = np.array([0,0,1])
+
 for j in range(len(dataset[:-1])):
 	if dataset[j,-1] == 1:
-		expected.append(np.array([1,0,0]))
+		expected.append(LA)
 	elif dataset[j,-1] == 2:
-		expected.append(np.array([0,1,0]))
+		expected.append(LB)
 	else:
-		expected.append(np.array([0,0,1]))
+		expected.append(LC)
 		
 expected = np.asarray(expected)
 
@@ -59,6 +64,8 @@ max = 1000
 Neural = ntk.Network(2, [8], 7, 3, fc.Sigmoid(), 0.01)
 
 iter = 1000
+pl_acc = []
+pl_rms = []
 while iter != 0:
 	iter -= 1
 	hits = 0
@@ -68,10 +75,16 @@ while iter != 0:
 		otp = Neural.layers[-1].cache
 		if np.array_equal(otp, expected[s]):
 			hits += 1
+		
 		rms += np.sum((otp-expected[s])**2)/3.0
 	print("\rTraining...\t\t\tEpoch: {}\tAccuracy: {}%\tCost: {}".format(max-iter, int(hits*100/len(input)), rms/len(input)),end="")
+	pl_acc.append(int(hits*100/len(input)))
+	pl_rms.append(rms/len(input))
 	
 ### Network Test data feed
+
+confusion = np.zeros((3,3))
+garbage = 0
 
 frms = 0
 facc = 0
@@ -79,9 +92,68 @@ for t in range(len(test)):
 	out = Neural.feed(test[t])
 	print("Exp:\t{}\tPred:\t{}\n".format(expected[t+167],out))
 	otp = Neural.layers[-1].cache
+	
+	#Succeds
 	if np.array_equal(otp, expected[s]):
 		facc += 1
+		if np.array_equal(otp, LA):
+			confusion[0][0] += 1
+		elif np.array_equal(otp, LB):
+			confusion[1][1] += 1
+		else:
+			confusion[2][2] += 1
+	
+	#Fails an A
+	elif np.array_equal(expected[s], LA):
+		if np.array_equal(otp, LB):
+			confusion[1][0] += 1
+		elif np.array_equal(otp, LC):
+			confusion[2][0] += 1
+	
+	#Fails a B
+	elif np.array_equal(expected[s], LB):
+		if np.array_equal(otp, LA):
+			confusion[1][0] += 1
+		elif np.array_equal(otp, LC):
+			confusion[1][2] += 1
+	
+	#Fails a C
+	elif np.array_equal(expected[s], LC):
+		if np.array_equal(otp, LB):
+			confusion[2][0] += 1
+		elif np.array_equal(otp, LB):
+			confusion[2][1] += 1
+	
+	# NN fails to assign a label
+	else:
+		garbage += 1
+	
+	
 	otp = Neural.layers[-1].cache
 	frms += np.sum((otp-expected[s])**2)/3.0
 		
-print("Test Results:\nAccuracy: {}\t RMS: {}".format(int(facc*100/len(test)), frms/len(test)))
+print("Test Results:\nAccuracy: {}\t RMS: {}\t Failed predictions: {}".format(int(facc*100/len(test)), frms/len(test), garbage))
+
+### Plots
+#Confusion matrix
+fig, ax = plt.subplots()
+im = ax.imshow(confusion)
+for i in range(3):
+	for j in range(3):
+		text = ax.text(j, i, confusion[i, j], ha="center", va="center", color="w")
+
+ax.set_title("")
+
+fig.tight_layout()
+plt.show()
+
+#Accuracy
+plt.plot(pl_acc)
+plt.ylabel("Accuracy '%'")
+plt.show()
+
+#RMS
+plt.plot(pl_rms)
+plt.ylabel("RMS")
+plt.show()
+
