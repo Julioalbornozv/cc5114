157bd4bae550f0ff733a7a8cd6719ebdd72157fc
Julioalbornozv <julio.albornoz@ug.uchile.cl>
2019-09-01 19:00:10 -0400

    Processor code refactoring 	+ Cleaned some sections of the code 	+ Created methods for each phase of the process 	+ Added docstring to each method 	+ Fixed some issues

--- Tarea 1/Processor.py
+++ Tarea 1/Processor.py
@@ -3,16 +3,9 @@ import Perceptron as pc
 import Functions as fc
 import numpy as np
 import matplotlib.pyplot as plt
-import pdb
 
-"""
-TODO: (Post-release)
-	+ Replace lists with numpy arrays
-	+ Generalize for multiple datasets
-	+ Modularize code and generalize inputs
-"""
 
-def Parse(path):
+def parse(path):
 	"""
 	Extracts data from a tab-separated txt file
 	
@@ -27,139 +20,144 @@ def Parse(path):
 	dataset = np.asarray(dataset)
 	return dataset
 	
-### Normalize data 
 
-dataset = Parse("seeds_dataset.txt")
-for i in range(len(dataset[0])-1):
-	dataset[:,i] = (dataset[:,i] - np.amin(dataset[:,i]))/(np.amax(dataset[:,i])-np.amin(dataset[:,i]))
+def prepare_data(dataset):
+	"""
+	Normalizes, splits and applies One-hot encoding to the dataset
+	
+	dataset: np.array containing the data
+	@returns Training and Test sets, each containing the input and the expected output of each row
+	@returns Labels obtained from the one-hot encoding process
+	"""
+	### Normalize data 
+	
+	height, width = dataset.shape
+	for i in range(width-1):
+		column = dataset[:,i]
+		dataset[:,i] = (column - np.amin(column))/(np.amax(column)-np.amin(column))
+	
 	np.random.shuffle(dataset)
-	
-### One-hot encoding
-expected = []
-LA = np.array([1,0,0])
-LB = np.array([0,1,0])
-LC = np.array([0,0,1])
-
-for j in range(len(dataset[:,-1])):
-	if dataset[j,-1] == 1:
-		expected.append(LA)
-	elif dataset[j,-1] == 2:
-		expected.append(LB)
-	else:
-		expected.append(LC)
 		
-expected = np.asarray(expected)
-
-### Input prep
-# 80% for training, 20% for test
-
-input, test = [], []
-split = len(dataset)*4/5
-
-for r in range(len(dataset)):
-	if r < split:
-		input.append((dataset[r][0:7].copy(), expected[r]))
-	else:
-		test.append((dataset[r][0:7].copy(), expected[r]))
+	### One-hot encoding
+	expected = np.zeros((height,3))
+	Lab = np.eye(3)
+	tag = dataset[:,-1] - 1
+	for j in range(height):
+		expected[j] = Lab[int(tag[j])]
 	
+	### Splits data
+	# 80% for training, 20% for test
 	
-input = np.asarray(input)
-test = np.asarray(test)
-
-### Network Training
-max = 500
-
-Neural = ntk.Network(2, [12], 7, 3, fc.Sigmoid(), 0.01)
-
-iter = 500
-pl_acc = []
-pl_rms = []
-while iter != 0:
-	iter -= 1
-	hits = 0
-	rms = 0.0
-	for s in range(len(input)):
-		Neural.train(input[s][0], expected[s])
-		otp = Neural.layers[-1].cache
-		if np.array_equal(otp, input[s][1]):
-			hits += 1
+	input, test = [], []
+	split = height*4/5
+	
+	for r in range(height):
+		if r < split:
+			input.append((dataset[r][0:7].copy(), expected[r]))
+		else:
+			test.append((dataset[r][0:7].copy(), expected[r]))
 		
-		rms += np.sum((otp-input[s][1])**2)/3.0
-	print("\rTraining...\t\t\tEpoch: {}\tAccuracy: {}%\tCost: {}".format(max-iter, int(hits*100/len(input)), rms/len(input)),end="")
-	pl_acc.append(int(hits*100/len(input)))
-	pl_rms.append(rms/len(input))
-	
-### Network Test data feed
-
-confusion = np.zeros((3,3))
-garbage = 0
-
-frms = 0
-facc = 0
-for t in range(len(test)):
-	out = Neural.feed(test[t][0])
-	print("Exp:\t{}\tPred:\t{}\n".format(test[t][1],out))
-	
-	#Succeds
-	if np.array_equal(out, test[t][1]):
-		#pdb.set_trace()
-		facc += 1
-		if np.array_equal(out, LA):
-			confusion[0][0] += 1
-		elif np.array_equal(out, LB):
-			confusion[1][1] += 1
-		else:
-			confusion[2][2] += 1
-	#Fails an A
-	elif np.array_equal(test[t][1], LA):
-		if np.array_equal(out, LB):
-			confusion[1][0] += 1
-		elif np.array_equal(out, LC):
-			confusion[2][0] += 1
-		else:
-			garbage += 1
-	#Fails a B
-	elif np.array_equal(test[t][1], LB):
-		if np.array_equal(out, LA):
-			confusion[0][1] += 1
-		elif np.array_equal(out, LC):
-			confusion[2][1] += 1
-		else:
-			garbage += 1
-	#Fails a C
-	elif np.array_equal(test[t][1], LC):
-		if np.array_equal(otp, LB):
-			confusion[0][2] += 1
-		elif np.array_equal(out, LC):
-			confusion[1][2] += 1
-		else:
-			garbage += 1
-	
-	otp = Neural.layers[-1].cache
-	frms += np.sum((otp-test[t][1])**2)/3.0
 		
-print("Test Results:\nAccuracy: {}%\t RMS: {}\t Failed predictions: {}".format(int(facc*100/len(test)), frms/len(test), garbage))
+	input = np.asarray(input)
+	test = np.asarray(test)
+	return input, test, Lab
 
-### Plots
-#Confusion matrix
-fig, ax = plt.subplots()
-im = ax.imshow(confusion)
-for i in range(3):
-	for j in range(3):
-		text = ax.text(j, i, confusion[i, j], ha="center", va="center", color="w")
+def train_NN(Neural, iter, input):
+	"""
+	Trains a neural network with its input set, returns training metrics
+	
+	Neural: Neural network to be trained
+	iter: Number of epochs for the training
+	input: Data being fed to the NN
+	
+	@return: acc: Accuracy over time
+	@return: rms: RMS over time
+	"""
+	### Network Training
+	max = iter
+	size = len(input)
+	
+	acc, rms = [], []
+	while iter != 0:
+		iter -= 1
+		hits, err = 0.0, 0.0
+		for s in range(size):
+			Neural.train(input[s][0], input[s][1])
+			out = Neural.layers[-1].cache
+			if np.array_equal(out, input[s][1]):
+				hits += 1
+			
+			err += np.sum((out-input[s][1])**2)/3.0
+		print("\rTraining...\t\t\tEpoch: {}\tAccuracy: {}%\tCost: {}".format(max-iter, int(hits*100/size), err/size),end="")
+		acc.append(int(hits*100/size))
+		rms.append(err/size)
+	
+	return acc, rms
+	
+def test_NN(Neural, test, Lab):
+	"""
+	Evaluates the network performance using the test set given
+	
+	Neural: Neural Network to be tested
+	test: Test set used in the evaluation
+	Lab: Labels obtained from the one-hot encoding process
+	
+	@return Confusion matrix
+	"""
+	print("\nTesting...")
+	conf = np.zeros((3,3))
+	
+	rms, acc, garbage = 0, 0, 0
+	size = len(test)
+	for t in range(size):
+		out = Neural.feed(test[t][0])
+		exp = test[t][1]
+		
+		if np.sum(out) == 1:
+			conf[np.nonzero(out)[0][0], np.nonzero(exp)[0][0]] += 1
 
-ax.set_title("")
+		rms += np.sum((out-exp)**2)/3.0
+			
+	acc = conf[0][0]+conf[1][1]+conf[2][2]
+	print("\n\n>> Test Results:\nAccuracy: {}%\t RMS: {}\t Undefined output: {}".format(int(acc*100/size), rms/size, int(size-np.sum(conf))))
+	return conf
+	
+def plot_results(Acc, RMS, confusion):
+	"""
+	Plots the confusion matrix obtained after a test, and the accuracy/rms from the training period
+	
+	Acc: Porcentual accuracy during training 
+	RMS: Error of the network during training
+	confusion: 3x3 matrix with the test results
+	"""
+	#Confusion matrix
+	fig, ax = plt.subplots()
+	im = ax.imshow(confusion)
+	for i in range(3):
+		for j in range(3):
+			text = ax.text(j, i, int(confusion[i, j]), ha="center", va="center", color="w")
+	
+	ax.set_title("")
+	
+	fig.tight_layout()
+	plt.show()
+	
+	#Accuracy
+	plt.plot(Acc)
+	plt.ylabel("Accuracy '%'")
+	plt.show()
+	
+	#RMS
+	plt.plot(RMS)
+	plt.ylabel("RMS")
+	plt.show()
 
-fig.tight_layout()
-plt.show()
 
-#Accuracy
-plt.plot(pl_acc)
-plt.ylabel("Accuracy '%'")
-plt.show()
+dataset = parse("seeds_dataset.txt")							#Data extraction
+input, test, Lab = prepare_data(dataset)						#Data curation
 
-#RMS
-plt.plot(pl_rms)
-plt.ylabel("RMS")
-plt.show()
+Neural = ntk.Network(2, [12], 7, 3, fc.Sigmoid(), 0.01)		#NN Structure 7-12-3 
+acc, rms = train_NN(Neural, 500, input)						#NN Training
 
+confusion = test_NN(Neural, test, Lab)							#NN Testing
+plot_results(acc, rms, confusion)								#Results
