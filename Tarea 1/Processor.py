d0aa075a525e57c379fe750c0ac2c0c9ac2b834e
Julioalbornozv <julio.albornoz@ug.uchile.cl>
2019-09-01 20:41:45 -0400

    Added example results graphs 	+ Added labels to graphs 	+ Added README.md mockup 	+ Fixed typo

--- Tarea 1/Processor.py
+++ Tarea 1/Processor.py
@@ -3,6 +3,7 @@ import Perceptron as pc
 import Functions as fc
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import cm
 
 
 def parse(path):
@@ -71,13 +72,13 @@ def train_NN(Neural, iter, input):
 	input: Data being fed to the NN
 	
 	@return: acc: Accuracy over time
-	@return: rms: RMS over time
+	@return: mse: Mean square error over time
 	"""
 	### Network Training
 	max = iter
 	size = len(input)
 	
-	acc, rms = [], []
+	acc, mse = [], []
 	while iter != 0:
 		iter -= 1
 		hits, err = 0.0, 0.0
@@ -90,9 +91,9 @@ def train_NN(Neural, iter, input):
 			err += np.sum((out-input[s][1])**2)/3.0
 		print("\rTraining...\t\t\tEpoch: {}\tAccuracy: {}%\tCost: {}".format(max-iter, int(hits*100/size), err/size),end="")
 		acc.append(int(hits*100/size))
-		rms.append(err/size)
+		mse.append(err/size)
 	
-	return acc, rms
+	return acc, mse
 	
 def test_NN(Neural, test, Lab):
 	"""
@@ -107,7 +108,7 @@ def test_NN(Neural, test, Lab):
 	print("\nTesting...")
 	conf = np.zeros((3,3))
 	
-	rms, acc, garbage = 0, 0, 0
+	mse, acc = 0, 0
 	size = len(test)
 	for t in range(size):
 		out = Neural.feed(test[t][0])
@@ -116,40 +117,53 @@ def test_NN(Neural, test, Lab):
 		if np.sum(out) == 1:
 			conf[np.nonzero(out)[0][0], np.nonzero(exp)[0][0]] += 1
 
-		rms += np.sum((out-exp)**2)/3.0
+		mse += np.sum((out-exp)**2)/3.0
 			
 	acc = conf[0][0]+conf[1][1]+conf[2][2]
-	print("\n\n>> Test Results:\nAccuracy: {}%\t RMS: {}\t Undefined output: {}".format(int(acc*100/size), rms/size, int(size-np.sum(conf))))
+	print("\n\n>> Test Results:\nAccuracy: {}%\t MSE: {}\t Undefined output: {}".format(int(acc*100/size), mse/size, int(size-np.sum(conf))))
 	return conf
 	
-def plot_results(Acc, RMS, confusion):
+def plot_results(Acc, MSE, confusion):
 	"""
-	Plots the confusion matrix obtained after a test, and the accuracy/rms from the training period
+	Plots the confusion matrix obtained after a test, and the accuracy/mse from the training period
 	
 	Acc: Porcentual accuracy during training 
-	RMS: Error of the network during training
+	MSE: Error of the network during training
 	confusion: 3x3 matrix with the test results
 	"""
 	#Confusion matrix
 	fig, ax = plt.subplots()
-	im = ax.imshow(confusion)
+	im = ax.imshow(confusion, cmap=cm.copper)
 	for i in range(3):
 		for j in range(3):
 			text = ax.text(j, i, int(confusion[i, j]), ha="center", va="center", color="w")
 	
-	ax.set_title("")
+	seeds = ["Predicted type 1", "Predicted type 2", "Predicted Type 3"]
+	exp_seeds = ["Type 1 seeds", "Type 2 seeds", "Type 3 seeds"]
+	ax.set_title("Confusion Matrix")
+	
+	ax.set_xticks(np.arange(len(exp_seeds)))
+	ax.set_yticks(np.arange(len(seeds)))
+	ax.set_xticklabels(exp_seeds)
+	ax.set_yticklabels(seeds)
 	
 	fig.tight_layout()
 	plt.show()
 	
 	#Accuracy
 	plt.plot(Acc)
-	plt.ylabel("Accuracy '%'")
+	plt.title("Accuracy during Training")
+	plt.axis([0, len(Acc)-1, 0, 100])
+	plt.xlabel("Epoch")
+	plt.ylabel("Accuracy (Percentage)")
 	plt.show()
 	
-	#RMS
-	plt.plot(RMS)
-	plt.ylabel("RMS")
+	#MSE
+	plt.plot(MSE)
+	plt.title("MSE during Training")
+	plt.axis([0, len(MSE)-1, 0, 1])
+	plt.xlabel("Epoch")
+	plt.ylabel("Mean Square Error")
 	plt.show()
 
 
@@ -157,7 +171,7 @@ dataset = parse("seeds_dataset.txt")							#Data extraction
 input, test, Lab = prepare_data(dataset)						#Data curation
 
 Neural = ntk.Network(2, [12], 7, 3, fc.Sigmoid(), 0.01)		#NN Structure 7-12-3 
-acc, rms = train_NN(Neural, 500, input)						#NN Training
+acc, mse = train_NN(Neural, 500, input)						#NN Training
 
 confusion = test_NN(Neural, test, Lab)							#NN Testing
-plot_results(acc, rms, confusion)								#Results
+plot_results(acc, mse, confusion)								#Results
---
+++ Tarea 1/README.md
@@ -0,0 +1,20 @@
+# Seed Clasifier
+
+Implementation of a Neural Network classifier for the [seeds dataset](https://archive.ics.uci.edu/ml/datasets/seeds) obtained from the UCI Machine Learning Repository
+
+## Prerequisites
+This project requires python 3.7 to execute properly. Libraries used include numpy and matplotlib.
+
+The Dataset analisis is done by executing the  Processor.py file
+
+## Analysis
+Lorem Ipsum
+
+## Example
+For this example we used a Neural Network with 7 inputs, a hidden layer with 12 neurons and an output layer of 3 neurons.
+
+![Training Accuracy](https://www.github.com/Julioalbornoz/cc5114/Tarea_1/Images/tr_acc.png)
+
+![Training Accuracy](https://www.github.com/Julioalbornoz/cc5114/Tarea_1/Images/tr_mse.png)
+
+![Confusion matrix](https://www.github.com/Julioalbornoz/cc5114/Tarea_1/Imagesconfusion.png)
